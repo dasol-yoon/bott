800e002b68dddaa27f0efa58e293f554516abb7b
dasol-yoon <dy327@cornell.edu>
2025-02-17 12:19:03 -0500

    introduce folder structures

---
+++ bott/__init__.py
@@ -0,0 +1,6 @@
+'''
+__init__.py 
+a special Python file that is executed when a directory is imported 
+as a package. Define which funcitons, classes, or variables will be available
+when the package is imported.
+'''
