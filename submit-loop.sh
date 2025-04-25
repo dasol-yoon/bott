c3aa34d46a81e20d780672ef2cd50cb1bd3da647
Poompol <pb482@cornell.edu>
2025-04-25 15:58:32 -0400

    modified job submission file

--- submit-loop.sh
+++ submit-loop.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-for trial in $(seq 1 1 1)
+for trial in $(seq 1 1 5)
 do
-    for algo in EI
+    for algo in EI KG EICF TS
     do
         for num_iter in 50
         do
-            for param_truth in "20 10 -10" "10 5 3"
+            for param_truth in "230 7 -3" 
             do
                 sbatch -J PB_${trial}_${algo} \
                        -o ./output/${trial}_${algo}_%j.out \
