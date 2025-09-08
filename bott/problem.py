0d76b13493449dcc2f53dd8ea0f7f9ddac20b583
dasol-yoon <dy327@cornell.edu>
2025-09-08 14:59:09 -0400

    scalar-> tensor for default scale factor

--- bott/problem.py
+++ bott/problem.py
@@ -58,7 +58,7 @@ class OptimizationProblem(SyntheticTestFunction):
         if scale_factor is not None: #20250903
             self.scaling_factor = scale_factor.to(device=self.device)
         else:
-            self.scaling_factor = self.get_scaling_factor(reduction_params)
+            self.scaling_factor = self.get_scaling_factor(reduction_params).to(device=self.device) #20250908
         self.safe_div_th_cnst = safe_div_th_cnst #20250904
         self.physics_model = simulate_cbed
         if reduction_params['reduction_type'] == 'square': #20250828
@@ -80,7 +80,7 @@ class OptimizationProblem(SyntheticTestFunction):
         tiles = temp_reduction_func(self.get_measurement_true())
         del rp['reduction_kwargs']['reduce'] # remove the reduce argument, so it doesn't affect the next call
         dim = tiles[0].shape #temporary fix for list type tiles
-        return np.sqrt(dim[-1]*dim[-2]) #tile height * tile width, number of pixels in each tile
+        return torch.Tensor([[np.sqrt(dim[-1]*dim[-2])]]) #tile height * tile width, number of pixels in each tile
     
     def get_physics_simu(self, *params, device_alt=None, params_abtem_alt=None): 
         #allows alternative parameters different from the initial ones for testing
