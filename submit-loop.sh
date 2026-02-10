1226624a846b7aa15babfde6f2446ffa4fd6f268
Poompol <pb482@cornell.edu>
2026-02-10 16:31:11 -0500

    update submit loop for submitting jobs

--- submit-loop.sh
+++ submit-loop.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-for trial in $(seq 50 1 50)
+for trial in $(seq 2 1 20)
 do
-    for algo in KG
+    for algo in EICF EI
     do
         for num_iter in 50
         do
-            for param_truth in "230 7 -3" 
+            for param_truth in "380 1.5 -1.5" 
             do
-                sbatch -J PB_${trial}_${algo} \
-                       -o ./output/${trial}_${algo}_%j.out \
-                       -e ./output/${trial}_${algo}_%j.err \
-                       --requeue submit.sub ${trial} ${algo} ${num_iter} "$param_truth"
+                sbatch -J run0_${trial}_${algo} \
+                       -o ./logs/${trial}_${algo}_%j.out \
+                       -e ./logs/${trial}_${algo}_%j.err \
+                       --requeue run_8.sub ${trial} ${algo} ${num_iter} "$param_truth"
                 sleep 0.1s
             done
         done
